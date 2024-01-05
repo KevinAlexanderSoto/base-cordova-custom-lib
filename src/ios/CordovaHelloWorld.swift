09c22ba41193483e8f39bb364213360fad205be5
kevin alexander soto <kevinalexandersoto999@gmail.com>
2024-01-05 12:43:01 -0500

    base cordova lib, content

---
+++ src/ios/CordovaHelloWorld.swift
@@ -0,0 +1,51 @@
+/********* CordovaHelloWorld.m Cordova Plugin Implementation *******/
+
+import Foundation
+import Cordova
+import UIKit
+
+@objc(CordovaHelloWorld)
+class CordovaHelloWorld: CDVPlugin {
+
+    @objc func sayHello(_ command: CDVInvokedUrlCommand) {
+         var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let msg = command.arguments[0] as? String ?? ""
+
+    if msg.characters.count > 0 {
+      let toastController: UIAlertController =
+        UIAlertController(
+          title: "",
+          message: msg,
+          preferredStyle: .alert
+        )
+      
+      self.viewController?.present(
+        toastController,
+        animated: true,
+        completion: nil
+      )
+
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        toastController.dismiss(
+            animated: true,
+            completion: nil
+        )
+      }
+        
+      pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: msg
+      )
+    }
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+    }
+
+    
