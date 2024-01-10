7e522260a8e0310f6e6cdfbd6433ed9d0c126e78
kevin alexander soto <kevinalexandersoto999@gmail.com>
2024-01-10 13:44:44 -0500

    feature: add JS brinde to comunicate whit the new impl

--- www/CordovaHelloWorld.js
+++ www/CordovaHelloWorld.js
@@ -3,3 +3,11 @@ var exec = require("cordova/exec");
 exports.sayHello = function (arg0, success, error) {
   exec(success, error, "CordovaHelloWorld", "sayHello", [arg0]);
 };
+
+exports.enable = function (success, error) {
+  exec(success, error, "CordovaHelloWorld", "enable");
+};
+
+exports.disable = function (arg0, success, error) {
+  exec(success, error, "CordovaHelloWorld", "disable");
+};
