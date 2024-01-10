6bea0db4758bdcb76ff3631bdbc83cdc572ff166
kevin alexander soto <kevinalexandersoto999@gmail.com>
2024-01-10 13:37:49 -0500

    feature: add code to enable and diable the screenshot in IOS

--- src/ios/CordovaHelloWorld.swift
+++ src/ios/CordovaHelloWorld.swift
@@ -1,51 +1,114 @@
 /********* CordovaHelloWorld.m Cordova Plugin Implementation *******/
 
 import Foundation
-import Cordova
 import UIKit
+import WebKit
 
 @objc(CordovaHelloWorld)
 class CordovaHelloWorld: CDVPlugin {
-
-    @objc func sayHello(_ command: CDVInvokedUrlCommand) {
-         var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
+    private var privacyViewController: UIViewController
+    private var isEnabled = false
+    private var pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_ERROR
     )
-
-    let msg = command.arguments[0] as? String ?? ""
-
-    if msg.characters.count > 0 {
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: "",
-          message: msg,
-          preferredStyle: .alert
-        )
-      
-      self.viewController?.present(
-        toastController,
-        animated: true,
-        completion: nil
-      )
-
-      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        toastController.dismiss(
-            animated: true,
-            completion: nil
-        )
-      }
+    override init() {
+        self.privacyViewController = CordovaHelloWorld.createSecureViewController()
         
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
+        super.init()
     }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-    }
-
     
+    override func pluginInitialize ( ){
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleDidBecomeActiveNotification),
+                                               name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleWillResignActiveNotification),
+                                               name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleDidChangeStatusBarOrientationNotification),
+                                               name: NSNotification.Name.UIApplicationDidChangeStatusBarOrientation, object: nil)
+        super.pluginInitialize()
+    }
+    
+    @objc public func enable(_ command: CDVInvokedUrlCommand) {
+        self.isEnabled = true
+        self.pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: "ScreenShot enable, can take screenshot"
+        )
+        
+        DispatchQueue.main.async {
+            self.webView?.disableScreenshots()
+            self.commandDelegate!.send(
+                self.pluginResult,
+                callbackId: command.callbackId
+            )
+        }
+        
+    }
+    
+    @objc public func disable(_ command: CDVInvokedUrlCommand) {
+        self.isEnabled = false
+        self.pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: "ScreenShot disable, can not take screenshot"
+        )
+        
+        DispatchQueue.main.async {
+            self.webView?.enableScreenshots()
+            self.commandDelegate!.send(
+                self.pluginResult,
+                callbackId: command.callbackId
+            )
+        }
+    }
+    
+    @objc private func handleDidBecomeActiveNotification ( ){
+        DispatchQueue.main.async {
+            self.privacyViewController.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+    @objc private func handleWillResignActiveNotification ( ){
+        DispatchQueue.main.async {
+            self.viewController?.present(self.privacyViewController, animated: false, completion: nil)
+        }
+    }
+    
+    @objc private func handleDidChangeStatusBarOrientationNotification ( ){
+        self.webView?.frame = CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+    }
+    
+    private static func createSecureViewController( ) -> UIViewController{
+        let privacyViewController = UIViewController()
+        privacyViewController.view.backgroundColor = UIColor.gray
+        privacyViewController.modalPresentationStyle = UIModalPresentationStyle.overFullScreen
+        return privacyViewController
+    }
+    
+}
+
+public extension UIView {
+    // Cannot be tested in simulator
+    func disableScreenshots() {
+        addSecureText()
+    }
+    
+    func addSecureText() {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        field.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(field)
+        field.centerYAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        field.centerXAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        self.layer.superlayer?.addSublayer(field.layer)
+        // Must be `last` for iOS 17
+        field.layer.sublayers?.last?.addSublayer(self.layer)
+    }
+    
+    func enableScreenshots() {
+        for view in self.subviews {
+            if let textField = view as? UITextField {
+                textField.isSecureTextEntry = false
+            }
+        }
+    }
+}
